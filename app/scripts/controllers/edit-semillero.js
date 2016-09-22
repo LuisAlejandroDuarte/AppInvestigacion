a66ef213b2b5c3060bb61e7f77176c00fc1077d7
lduarte <ladm4@hotmail.com>
2016-09-22 17:31:36 -0500

    Semilleros

--- app/scripts/controllers/edit-semillero.js
+++ app/scripts/controllers/edit-semillero.js
@@ -3,22 +3,6 @@
 angular.module('listaTareasApp')
   .controller('editSemillero', function($scope,$location,datosSemillero,TareasResource,$route,$window) {
    
-  	 var user = JSON.parse($window.sessionStorage.getItem('investigador'));
-
-	if (user==null || user==undefined)
-    {
-
-      $location.path('/inicio');
-      return;
-    }
-    else
-    {
-    	$scope.$parent.mnuInvestiga =false;
-            $scope.$parent.mnuAdmin = false;
-            $scope.$parent.mnuConvocatoria = false;     
-    }
-
-
     $scope.jqxPanelSettings =
       {
         height: '200',
@@ -27,5 +11,31 @@ angular.module('listaTareasApp')
         width:'auto',
         sizeMode:'fixed'
       }
-     	
+
+
+  	 var user = JSON.parse($window.sessionStorage.getItem('investigador'));
+
+  	if (user==null || user==undefined)
+      {
+
+        $location.path('/inicio');
+        return;
+      }
+      else
+      {
+      	      $scope.$parent.mnuInvestiga =false;
+              $scope.$parent.mnuAdmin = false;
+              $scope.$parent.mnuConvocatoria = false;     
+
+              $scope.mostrarDatosInvestigador(user[0].Id);
+
+      }
+
+
+  
+       $scope.mostrarDatosInvestigador= function(id);
+       {
+
+       }
+
 });
