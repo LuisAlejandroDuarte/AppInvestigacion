90ebb36d34ba270014d14f1c4937da4203c947bb
lduarte2 <ladm42@hotmail.com>
2016-09-24 00:09:01 -0500

    Semillero

--- app/scripts/controllers/edit-semillero.js
+++ app/scripts/controllers/edit-semillero.js
@@ -2,9 +2,26 @@
 
 angular.module('listaTareasApp')
   .controller('editSemillero', function($scope,$location,datosSemillero,TareasResource,$route,$window) {
-   
+   	
+   	    $scope.jqxPanelSettings =
+      {
+        height: '200',
+        autoUpdate:true,
+        theme:'bootstrap',
+        width:'auto',
+        sizeMode:'fixed'
+      }
+
   	 var user = JSON.parse($window.sessionStorage.getItem('investigador'));
 
+
+    $scope.mostrarDatosInvestigador = function(id)
+    {
+    	var datos = {
+    		Accion:'S',
+    		SQL:'SELECT '
+    	}
+    }
 	if (user==null || user==undefined)
     {
 
@@ -13,19 +30,16 @@ angular.module('listaTareasApp')
     }
     else
     {
-    	$scope.$parent.mnuInvestiga =false;
+    	    $scope.$parent.mnuInvestiga =false;
             $scope.$parent.mnuAdmin = false;
             $scope.$parent.mnuConvocatoria = false;     
+            $scope.mostrarDatosInvestigador(user.INV_CODI);
     }
 
+   
 
-    $scope.jqxPanelSettings =
-      {
-        height: '200',
-        autoUpdate:true,
-        theme:'bootstrap',
-        width:'auto',
-        sizeMode:'fixed'
-      }
+
+
+
      	
 });
