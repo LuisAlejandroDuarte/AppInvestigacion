a60b8ddaa37039cb71011a2152db985ce9223c40
lduarte <ladm4@hotmail.com>
2016-09-29 16:54:30 -0500

    Semillero

--- app/scripts/controllers/edit-semillero.js
+++ app/scripts/controllers/edit-semillero.js
@@ -3,7 +3,7 @@
 angular.module('listaTareasApp')
   .controller('editSemillero', function($scope,$location,datosSemillero,TareasResource,$route,$window) {
 
-   	
+   	 $('#myModal').hide();
   	  moment.locale('es');
   		$scope.semillero = {
 
@@ -13,7 +13,8 @@ angular.module('listaTareasApp')
   			strVision:'',
   			strObjetivo:'',
   			strProyeccionRegional:'',
-  			strTematicaInvestigacion:''
+  			strTematicaInvestigacion:'',
+        avaluado:1
 
   		}
   		  var idSemillero = ($route.current.params.idSemillero) ? parseInt($route.current.params.idSemillero) :0 ;
@@ -74,17 +75,37 @@ angular.module('listaTareasApp')
 
     }
 
-    $scope.mostrarDatosSemillero(idSemillero)
+    $scope.mostrarDatosSemillero = function(idSemillero)
     {
+
     	var datos = {
     		Accion:'S',
-    		SQL:'SELECT SEM_NOMB,S.SEM_MISI,S.SEM_VISI,' +
-    			' S.SEM_OBJE,S.SEM_PROY_REGI,S.SEM_TEMA_INVE ' + 
-    			' FROM sgi_semi AS S INNER JOIN sgi_inve_semi ' + 
-    			' AS SI ON SI.ins_semi_codi = S.SEM_CODI ' +
-    			'  '
-
+    		SQL:'SELECT SEM_NOMB,SEM_MISI,SEM_VISI,' +
+    			' SEM_OBJE,SEM_PROY_REGI,SEM_TEMA_INVE,SEM_FECH_CREA,SEM_AVAL_UNAD ' + 
+    			' FROM sgi_semi WHERE SEM_CODI =' + idSemillero    			
     	}
+
+      var select = TareasResource.SQL(datos);
+
+          select.then(function(result){
+
+              $scope.semillero.strNombre = result.data[0].SEM_NOMB;
+              $scope.semillero.strMision = result.data[0].SEM_MISI;
+              $scope.semillero.strVision = result.data[0].SEM_VISI;
+              $scope.semillero.strObjetivo= result.data[0].SEM_OBJE;
+              $scope.semillero.strProyeccionRegional= result.data[0].SEM_PROY_REGI;
+              $scope.semillero.strTematicaInvestigacion= result.data[0].SEM_TEMA_INVE;
+              $scope.semillero.avaluado =  result.data[0].SEM_AVAL_UNAD; 
+               var day = moment(result.data[0].SEM_FECH_CREA).format("D");
+               var mounth = moment(result.data[0].SEM_FECH_CREA).format("M");
+               var year = moment(result.data[0].SEM_FECH_CREA).format("YYYY");
+
+               var  fechaStr = year + "," + mounth + "," + day;
+
+              $scope.semillero.strFecha = new Date(fechaStr);
+
+          })
+
     }
 
 
@@ -109,37 +130,74 @@ angular.module('listaTareasApp')
    }
 
     $scope.onClickGuardar = function(){
-
+          $('#myModal').show();
     	if (idSemillero==0)
     	{
     		var datos = {
 
     			Accion:'I',
     			SQL:"INSERT INTO sgi_semi (" + 
-    				" sem_nomb,sem_misi,sem_visi,sem_obje,sem_proy_regi,sem_tema_inve) " +
-    				" VALUES ('" + $scope.semillero.strNombre + "','"  +  $scope.semillero.strMision + "','" + $scope.semillero.strVision + "','" + $scope.semillero.strObjetivo + "','" + $scope.semillero.strProyeccionRegional + "','" + $scope.semillero.strTematicaInvestigacion + "')" 
+    				" sem_nomb,sem_misi,sem_visi,sem_obje,sem_proy_regi," + 
+            " sem_tema_inve,sem_fech_crea,sem_aval_unad) " +
+    				" VALUES ('" + $scope.semillero.strNombre + "'," +
+            " '"  +  $scope.semillero.strMision + "'," + 
+            " '" + $scope.semillero.strVision + "'," + 
+            " '" + $scope.semillero.strObjetivo + "'," +
+            " '" + $scope.semillero.strProyeccionRegional + "'," +
+            " '" + $scope.semillero.strTematicaInvestigacion + "'," +
+            " '" + moment($scope.semillero.strFecha).format("YYYY-MM-DD") + "', " +
+            " " + $scope.semillero.avaluado + ")" 
 
     		}
 
     		var insertSemilero =  TareasResource.SQL(datos);
     		insertSemilero.then(function(result){
 
+            datos = {
+              Accion:"I",
+              SQL :"INSERT INTO sgi_inve_semi (INS_INVE_IDEN,INS_SEMI_CODI) " +
+              " VALUES (" + user.INV_CODI + "," + result.data[0].valor + ")"
 
-    			var IdSemilla = result.data[0].valor;
+            }
+              insertSemilero =  TareasResource.SQL(datos);
+              insertSemilero.then(function(result){
 
-    			datos = {
-    				Accion:'I',
-    				SQL:"INSERT INTO sgi_inve_semi (ins_inve_iden,ins_semi_codi,ins_fech_inic) " +
-    					" VALUES (" + user.INV_CODI + "," + IdSemilla + ",'" + moment($scope.semillero.strFecha).format('YYYY-MM-DD') + "')"
-    				}
-    				 insertSemilero =  TareasResource.SQL(datos);
-    					insertSemilero.then(function(result){
+                  $('#myModal').hide();
+                  $window.alert("Semillero Guardado");
+                   $location.path('/semillero');                    
 
-
-
-    					});
+              });    	
     		});
     	}
+
+      else
+      {
+        datos = {
+
+          Accion :"S",
+          SQL:"UPDATE sgi_semi set sem_nomb ='" + $scope.semillero.strNombre + "'," + 
+          " sem_misi ='"  +  $scope.semillero.strMision + "', " +
+          " sem_visi ='" + $scope.semillero.strVision + "'," + 
+          " sem_obje ='" + $scope.semillero.strObjetivo + "'," +
+          " sem_proy_regi ='" + $scope.semillero.strProyeccionRegional + "'," + 
+          " sem_tema_inve = '" + $scope.semillero.strTematicaInvestigacion + "'," +
+          " sem_fech_crea = '" + moment($scope.semillero.strFecha).format("YYYY-MM-DD") + "'," +
+          " sem_aval_unad = " + $scope.semillero.avaluado + " WHERE SEM_CODI = " + idSemillero            
+
+        }
+
+         insertSemilero =  TareasResource.SQL(datos);
+              insertSemilero.then(function(result){
+
+                  $('#myModal').hide();
+                  $window.alert("Semillero Actualizado");
+                   $location.path('/semillero');                 
+
+              });     
+
+      }
+
+
     }
      	
 });
